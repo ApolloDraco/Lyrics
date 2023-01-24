8f28d07f19d6f25e4c486befa6af36becc539179
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-01-24 17:10:41 -0500

    Create Lyrics.cs

---
+++ Lyrics.cs
@@ -0,0 +1,17 @@
+/* 
+ * Description: 
+ * Write, compile, and test a program named Lyrics that displays at least four lines of your favorite song.
+ */
+
+using System;
+using static System.Console;
+class PersonalInfo
+{
+   static void Main()
+   {
+      WriteLine("Lucinda Potter");
+      WriteLine("6/24/1992");
+      WriteLine("work 000-555-0101");
+      WriteLine("cell 000-555-0189");
+   }
+}
